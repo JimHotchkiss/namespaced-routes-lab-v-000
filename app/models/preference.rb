2b410b05889b126d5574719dad2f31a2fdb92bcc
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-09-22 18:36:28 +0000

    Done.

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,3 +1,9 @@
-class Perference < ActiveRecord::Base
-  
+class Preference < ActiveRecord::Base
+  attr_accessor :artist_sort_order, :song_sort_order, :allow_create_songs, :allow_create_artists 
+
+
+
+
+
+
 end
